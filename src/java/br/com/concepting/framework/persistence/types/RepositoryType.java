21475aaf2cdd4d3ccca5ffa0ed8a366cc39d1c10
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-09 19:35:40 -0200

    Code improvement.

--- src/java/br/com/concepting/framework/persistence/types/RepositoryType.java
+++ src/java/br/com/concepting/framework/persistence/types/RepositoryType.java
@@ -1,67 +1,62 @@
 package br.com.concepting.framework.persistence.types;
 
-import br.com.concepting.framework.util.interfaces.IEnum;
-
 /**
  * Classe que define as constantes para os tipos de repositórios de persistência.
  * 
  * @author fvilarinho
  * @since 1.0
  */
-public enum RepositoryType implements IEnum{
+public enum RepositoryType{
 	/**
 	 * Constante que define o repositório para o servidor MS-SQL Server.
 	 */
-	MSSQL("mssql", "MS-SQL Server", 1433),
+	MSSQL("MS-SQL Server", 1433),
 
 	/**
 	 * Constante que define o repositório para o servidor Sybase.
 	 */
-	SYBASE("sybase", "Sybase ASE/ASA", 2368),
+	SYBASE("Sybase ASE/ASA", 2368),
 
 	/**
 	 * Constante que define o repositório para o servidor DB2.
 	 */
-	DB2("db2", "IBM DB2", 50000),
+	DB2("IBM DB2", 50000),
 
 	/**
 	 * Constante que define o repositório para o servidor Oracle.
 	 */
-	ORACLE("oracle", "Oracle", 1521),
+	ORACLE("Oracle", 1521),
 
 	/**
 	 * Constante que define o repositório para o servidor MySQL.
 	 */
-	MYSQL("mysql", "MySQL", 3306),
+	MYSQL("MySQL", 3306),
 
 	/**
 	 * Constante que define o repositório para o servidor PostgreSQL.
 	 */
-	POSTGRESQL("postgresql", "PostgreSQL", 5432),
+	POSTGRESQL("PostgreSQL", 5432),
 
 	/**
 	 * Constante que define o repositório para o servidor Firebird.
 	 */
-	FIREBIRD("firebird", "Firebird", 3050),
+	FIREBIRD("Firebird", 3050),
 	
 	/**
 	 * Constante que define o repositório para o servidor Informix.
 	 */
-	INFORMIX("informix", "Informix", 1532);
+	INFORMIX("Informix", 1532);
 
-	private Object  key;
-	private String  description;
-	private Integer defaultPort;
+	private String  description = "";
+	private Integer defaultPort = 0;
 
 	/**
 	 * Construtor - Define o valor da constante.
 	 * 
-	 * @param key String contendo o valor desejado.
 	 * @param description String contendo a descrição do repositório.
 	 * @param defaultPort Valor inteiro contendo a porta padrão.
 	 */
-	private RepositoryType(String key, String description, Integer defaultPort){
-		setKey(key);
+	private RepositoryType(String description, Integer defaultPort){
 		setDescription(description);
 		setDefaultPort(defaultPort);
 	}
@@ -101,54 +96,4 @@ public enum RepositoryType implements IEnum{
 	public void setDefaultPort(Integer defaultPort){
     	this.defaultPort = defaultPort;
     }
-	
-	/**
-	 * @see br.com.concepting.framework.util.interfaces.IEnum#getKey()
-	 */
-    public <O> O getKey(){
-		return (O)key;
-	}
-
-	/**
-	 * @see br.com.concepting.framework.util.interfaces.IEnum#setKey(java.lang.Object)
-	 */
-	public <O> void setKey(O key){
-		this.key = (String)key;
-	}
-
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString(){
-		return key.toString();
-	}
-
-	/**
-	 * @see br.com.concepting.framework.util.interfaces.IEnum#toEnum(java.lang.Object)
-	 */
-	public <O> IEnum toEnum(O value) throws IllegalArgumentException{
-		return toRepositoryType((String)value);
-	}
-
-	/**
-	 * Converte uma string em uma instância da constante.
-	 * 
-	 * @param value String contendo o valor desejado.
-	 * @return Instância da constante.
-	 */
-	public static RepositoryType toRepositoryType(String value) throws IllegalArgumentException{
-		if(value == null)
-			throw new IllegalArgumentException();
-
-		try{
-			return valueOf(value.toUpperCase());
-		}
-		catch(Throwable e){
-			for(RepositoryType constant : values())
-				if(value.equals(constant.getDescription()))
-					return constant;
-			
-			throw new IllegalArgumentException(value);
-		}
-	}
 }
