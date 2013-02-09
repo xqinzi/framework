0b9ff680992afd27de2699f23f4f72bd1abb0a91
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-09 19:14:28 -0200

    Code improvement.

--- src/java/br/com/concepting/framework/web/taglibs/CloseButtonTag.java
+++ src/java/br/com/concepting/framework/web/taglibs/CloseButtonTag.java
@@ -1,4 +1,34 @@
 package br.com.concepting.framework.web.taglibs;
 
+/**
+ * Classe que define o componente visual para o botão com a função de fechar uma caixa de diálogo.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
 public class CloseButtonTag extends ButtonTag{
+    /**
+     * @see br.com.concepting.framework.web.taglibs.ButtonTag#initialize()
+     */
+    public void initialize() throws Throwable{
+        String        onClick        = getOnClick();
+        StringBuilder onClickContent = new StringBuilder();
+
+        if(onClick.length() > 0){
+            onClickContent.append(onClick);
+            
+            if(getParent() instanceof DialogBoxTag)
+                onClickContent.append("; ");
+        }
+        
+        if(getParent() instanceof DialogBoxTag){
+            onClickContent.append("hideDialogBox('");
+            onClickContent.append(getName());
+            onClickContent.append("');");
+            
+            setOnClick(onClickContent.toString());
+        }    
+        
+        super.initialize();
+    }
 }
