641caffbc2bc1767178ea582fd85110376716288
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-10 16:37:32 -0200

    Code improvement.

--- src/java/br/com/concepting/framework/web/taglibs/UndoButtonTag.java
+++ src/java/br/com/concepting/framework/web/taglibs/UndoButtonTag.java
@@ -1,6 +1,6 @@
 package br.com.concepting.framework.web.taglibs;
 
-import br.com.concepting.framework.web.types.ComponentType;
+import br.com.concepting.framework.util.types.ComponentType;
 
 /**
  * Classe que define o componente visual para o botão com a função "undo" ou "reset".
