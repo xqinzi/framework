f533dcb6c1994b0389eea0def6709297c701f1aa
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-10 18:49:20 -0200

    Documentation.

--- src/java/br/com/concepting/framework/security/model/HostModel.java
+++ src/java/br/com/concepting/framework/security/model/HostModel.java
@@ -4,7 +4,13 @@ import br.com.concepting.framework.model.BaseModel;
 import br.com.concepting.framework.model.annotations.Model;
 import br.com.concepting.framework.model.annotations.Property;
 
-@Model
+/**
+ * Classe que define o modelo de dados que armazena as propriedades de um host.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+@Model 
 public class HostModel extends BaseModel{
     @Property(isIdentity=true)
     private Integer id = 0;
@@ -15,26 +21,56 @@ public class HostModel extends BaseModel{
     @Property(isForSearch=true)
     private String ip = "";
 
+    /**
+     * Retorna o identificador do host.
+     * 
+     * @return Valor inteiro contendo o identificador do host.
+     */
     public Integer getId(){
         return id;
     }
 
+    /**
+     * Define o identificador do host.
+     * 
+     * @param id Valor inteiro contendo o identificador do host.
+     */
     public void setId(Integer id){
         this.id = id;
     }
 
+    /**
+     * Retorna o nome do host.
+     * 
+     * @return String contendo o nome do host.
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Define o nome do host.
+     * 
+     * @param naem String contendo o nome do host.
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Retorna o IP do host.
+     * 
+     * @return String contendo o IP do host.
+     */
     public String getIp(){
         return ip;
     }
 
+    /**
+     * Define o IP do host.
+     * 
+     * @param ip String contendo o IP do host.
+     */
     public void setIp(String ip){
         this.ip = ip;
     }
