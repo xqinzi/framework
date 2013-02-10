58cb91623cac175aa124e17fcc08477082a6d7fa
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-09 22:02:45 -0200

    Code improvement.

---
+++ src/java/br/com/concepting/framework/util/helpers/DateTime.java
@@ -0,0 +1,12 @@
+package br.com.concepting.framework.util.helpers;
+
+import java.util.Date;
+
+/**
+ * Classe que define uma data/horário.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public class DateTime extends Date{
+}
