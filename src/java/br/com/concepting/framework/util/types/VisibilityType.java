0352bc53c578b5817c44fac00d63d1dda3aea14c
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-10 17:45:46 -0200

    Code improvement.

---
+++ src/java/br/com/concepting/framework/util/types/VisibilityType.java
@@ -0,0 +1,19 @@
+package br.com.concepting.framework.util.types;
+
+/**
+ * Classe que define as constantes dos modos de visibilidade de um objeto/componente.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public enum VisibilityType{
+    /**
+     * Constante que define que o objeto/componente está escondido (não visível).
+     */
+    HIDDEN,
+    
+    /**
+     * Constante que define que o objeto/componente está visível.
+     */
+    VISIBLE;
+}
