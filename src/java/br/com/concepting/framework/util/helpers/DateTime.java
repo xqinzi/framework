9102907b14bcde0531c6678b6e56e743bfb70644
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-13 09:16:32 -0200

    Code improvement.

--- src/java/br/com/concepting/framework/util/helpers/DateTime.java
+++ src/java/br/com/concepting/framework/util/helpers/DateTime.java
@@ -9,4 +9,20 @@ import java.util.Date;
  * @since 3.0
  */
 public class DateTime extends Date{
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     */
+    public DateTime(){
+        super();
+    }
+
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     * 
+     * @param milliseconds Valor numérico contendo a representação (em milisegundos) 
+     * da data/horário desejado.
+     */
+    public DateTime(long milliseconds){
+        super(milliseconds);
+    }
 }
