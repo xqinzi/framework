c5986b7900a2a4629d2cce758ab5101fa63218b1
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-02-16 20:26:41 -0200

    Code improvement.

---
+++ src/java/br/com/concepting/framework/exceptions/InternalErrorException.java
@@ -0,0 +1,44 @@
+package br.com.concepting.framework.exceptions;
+
+/**
+ * Classe que define a exceção para erros internos da aplicação.
+ * 
+ * @author fvilarinho
+ * @since 1.0
+ */
+public class InternalErrorException extends Exception{
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     */
+    public InternalErrorException(){
+        super();
+    }
+    
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     * 
+     * @param messageKey String contendo o identificador da chave da mensagem.
+     */
+	public InternalErrorException(String messageKey){
+		super(messageKey);
+	}
+
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     * 
+     * @param exception Instância da exceção a ser encapsulada.
+     */
+	public InternalErrorException(Throwable exception){
+		super(exception);
+	}
+	
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     * 
+     * @param messageKey String contendo o identificador da chave da mensagem.
+     * @param exception Instância da exceção a ser encapsulada.
+     */
+	public InternalErrorException(String messageKey, Throwable exception){
+	    super(messageKey, exception);
+	}
+}
