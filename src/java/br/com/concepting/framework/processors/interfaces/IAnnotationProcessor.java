ccb784290a24fb5023ff25578b917998400b7b08
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-03-10 19:31:07 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/processors/interfaces/IAnnotationProcessor.java
@@ -0,0 +1,14 @@
+package br.com.concepting.framework.processors.interfaces;
+
+/**
+ * Classe que define a interface dos processadores de anotação.
+ * 
+ * @author fvilarinho
+ * @since 4.0
+ */
+public interface IAnnotationProcessor{
+    /**
+     * Inicia o processamento da anotação.
+     */
+    public void process();    
+}
