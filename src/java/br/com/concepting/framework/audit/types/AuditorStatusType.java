4669c5182edcaad1a72b64b06dff56a7ca5dc951
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-03-10 15:29:31 -0300

    Code improvement.

--- src/java/br/com/concepting/framework/audit/types/AuditorStatusType.java
+++ src/java/br/com/concepting/framework/audit/types/AuditorStatusType.java
@@ -7,20 +7,50 @@ package br.com.concepting.framework.audit.types;
  * @since 1.0
  */
 public enum AuditorStatusType{
-	/**
-	 * Constante que define o identificador da mensagem de início de processamento.
-	 */
-	INITIALIZING,
-
+    /**
+     * Constante que define o identificador da mensagem de inicialização do 
+     * processamento.
+     */
+    INIT("init"),
+    
 	/**
 	 * Constante que define o identificador da mensagem quando não ocorreram erros 
 	 * no processamento.
 	 */
-	PROCESSED_WITHOUT_ERROR,
+	PROCESSED("processed"),
 
 	/**
 	 * Constante que define o identificador da mensagem quando ocorreram erros no 
 	 * processamento.
 	 */
-	PROCESSED_WITH_ERROR;
+	PROCESSED_WITH_ERROR("error");
+    
+    private String description = "";
+    
+    /**
+     * Construtor - Define a descrição da constante.
+     * 
+     * @param description String contendo a descrição.
+     */
+    private AuditorStatusType(String description){
+        setDescription(description);
+    }
+
+    /**
+     * Retorna a descrição da constante.
+     * 
+     * @return String contendo a descrição.
+     */
+    public String getDescription(){
+        return description;
+    }
+
+    /**
+     * Define a descrição da constante.
+     * 
+     * @param description String contendo a descrição.
+     */
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
