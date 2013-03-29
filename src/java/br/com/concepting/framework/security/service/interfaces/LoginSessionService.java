50e8327f727f5bf95c4a7f66c88fc9546c5cb567
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-03-29 15:53:59 -0300

    Code improvement.

--- src/java/br/com/concepting/framework/security/service/interfaces/LoginSessionService.java
+++ src/java/br/com/concepting/framework/security/service/interfaces/LoginSessionService.java
@@ -21,7 +21,7 @@ import br.com.concepting.framework.security.model.LoginSessionModel;
 import br.com.concepting.framework.security.model.UserModel;
 import br.com.concepting.framework.service.annotations.Service;
 import br.com.concepting.framework.service.interfaces.IService;
-import br.com.concepting.framework.service.types.ServiceTransactionType;
+import br.com.concepting.framework.util.types.TransactionType;
 
 /**
  * Interface contendo as assinaturas dos métodos que implementam as regras de negócio que envolvem 
@@ -30,10 +30,10 @@ import br.com.concepting.framework.service.types.ServiceTransactionType;
  * @author fvilarinho
  * @since 1.0
  */
-@Auditable(entityId="loginSession")
+@Auditable
 public interface LoginSessionService extends IService{
-    @Auditable(businessId="sendForgottenPassword")
-    @Service(transactionType=ServiceTransactionType.READ_WRITE)
+    @Auditable
+    @Service(transactionType=TransactionType.READ_WRITE)
     public <L extends LoginSessionModel, U extends UserModel> void sendForgottenPassword(L loginSession) throws UserNotFoundException, UserBlockedException, InternalErrorException, RemoteException;
     
     /**
@@ -50,8 +50,8 @@ public interface LoginSessionService extends IService{
      * @throws InternalErrorException
      * @throws RemoteException
      */
-	@Auditable(businessId="logIn")
-    @Service(transactionType=ServiceTransactionType.READ_WRITE)
+	@Auditable
+    @Service(transactionType=TransactionType.READ_WRITE)
 	public <L extends LoginSessionModel, U extends UserModel, S extends SystemModuleModel, SS extends SystemSessionModel, LP extends LoginParametersModel, A extends AccessListModel, H extends HostModel> void logIn(L loginSession) throws UserAlreadyLoggedInException, UserNotFoundException, UserBlockedException, ExpiredPasswordException, PasswordWillExpireException, InvalidPasswordException, PermissionDeniedException, InternalErrorException, RemoteException;
 	
 	/**
@@ -62,8 +62,8 @@ public interface LoginSessionService extends IService{
 	 * @throws InternalErrorException
 	 * @throws RemoteException
 	 */
-	@Auditable(businessId="changePassword")
-    @Service(transactionType=ServiceTransactionType.READ_WRITE)
+	@Auditable
+    @Service(transactionType=TransactionType.READ_WRITE)
 	public <L extends LoginSessionModel, U extends UserModel> void changePassword(L loginSession) throws PasswordsNotMatchException, InternalErrorException, RemoteException;
 
 	/**
@@ -73,7 +73,7 @@ public interface LoginSessionService extends IService{
 	 * @throws InternalErrorException
 	 * @throws RemoteException
 	 */
-    @Auditable(businessId="logOut")
-    @Service(transactionType=ServiceTransactionType.READ_WRITE)
+    @Auditable
+    @Service(transactionType=TransactionType.READ_WRITE)
     public <L extends LoginSessionModel> void logOut(L loginSession) throws InternalErrorException, RemoteException;
 }
