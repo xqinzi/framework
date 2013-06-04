0d3a09c121f1da1dd16317805f5e30da58c5d9e7
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-06-04 10:19:50 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/web/taglibs/TimerTag.java
@@ -0,0 +1,177 @@
+package br.com.concepting.framework.web.taglibs;
+
+import br.com.concepting.framework.util.StringUtil;
+
+public class TimerTag extends BaseActionFormElementTag{
+    private Integer milliseconds       = 0;
+    private String  action             = "";
+    private String  forward            = "";
+    private String  forwardOnFail      = "";
+    private String  updateViews        = "";
+    private Boolean validate           = false;
+    private String  validateProperties = "";
+    
+    public Integer getMilliseconds(){
+        return milliseconds;
+    }
+    
+    public void setMilliseconds(Integer milliseconds){
+        this.milliseconds = milliseconds;
+    }
+    
+    public String getAction(){
+        return action;
+    }
+    
+    public void setAction(String action){
+        this.action = action;
+    }
+    
+    public String getForward(){
+        return forward;
+    }
+    
+    public void setForward(String forward){
+        this.forward = forward;
+    }
+    
+    public String getForwardOnFail(){
+        return forwardOnFail;
+    }
+    
+    public void setForwardOnFail(String forwardOnFail){
+        this.forwardOnFail = forwardOnFail;
+    }
+    
+    public String getUpdateViews(){
+        return updateViews;
+    }
+    
+    public void setUpdateViews(String updateViews){
+        this.updateViews = updateViews;
+    }
+    
+    public Boolean getValidate(){
+        return validate;
+    }
+    
+    public void setValidate(Boolean validate){
+        this.validate = validate;
+    }
+    
+    public String getValidateProperties(){
+        return validateProperties;
+    }
+
+    public void setValidateProperties(String validateProperties){
+        this.validateProperties = validateProperties;
+    }
+    
+    public void render() throws Throwable{
+        ScriptTag scriptTag = new ScriptTag();
+        
+        scriptTag.setPageContext(pageContext);
+        
+        String        name    = getName();
+        StringBuilder content = new StringBuilder();
+        
+        content.append("function ");
+        content.append(name);
+        content.append("TimerAction(){");
+        content.append(StringUtil.getLineBreak());
+        
+        String actionForm = getActionForm();
+                
+        if(actionForm.length() == 0){
+            content.append(action);
+            
+            if(!action.endsWith(";"))
+                content.append(";");
+        }
+        else{
+            content.append("document.");
+            content.append(actionForm);
+            content.append(".action.value = '");
+            content.append(action);
+            content.append(";");
+            content.append(StringUtil.getLineBreak());
+            
+            if(forward.length() > 0){
+                content.append("document.");
+                content.append(actionForm);
+                content.append(".forward.value = '");
+                content.append(forward);
+                content.append("';");
+                content.append(StringUtil.getLineBreak());
+            }
+
+            if(forwardOnFail.length() > 0){
+                content.append("document.");
+                content.append(actionForm);
+                content.append(".forwardOnFail.value = '");
+                content.append(forwardOnFail);
+                content.append("';");
+                content.append(StringUtil.getLineBreak());
+            }
+            
+            if(updateViews.length() > 0){
+                content.append("document.");
+                content.append(actionForm);
+                content.append(".updateViews.value = '");
+                content.append(updateViews);
+                content.append("';");
+                content.append(StringUtil.getLineBreak());
+            }
+            
+            content.append("document.");
+            content.append(actionForm);
+            content.append(".validate.value = ");
+            content.append(validate);
+            content.append(";");
+            content.append(StringUtil.getLineBreak());
+            
+            if(validateProperties.length() > 0){
+                content.append("document.");
+                content.append(actionForm);
+                content.append(".validateProperties.value = '");
+                content.append(validateProperties);
+                content.append("';");
+                content.append(StringUtil.getLineBreak());
+            }
+            
+            content.append("submitForm(document.");
+            content.append(actionForm);
+            content.append(");");
+            content.append(StringUtil.getLineBreak());
+        }
+        
+        content.append("}");
+        content.append(StringUtil.getLineBreak());
+        content.append(StringUtil.getLineBreak());
+        content.append("setInterval(");
+        content.append(name);
+        content.append("TimerAction, ");
+        content.append(milliseconds);
+        content.append(");");
+        content.append(StringUtil.getLineBreak());
+
+        scriptTag.setContent(content.toString());
+        scriptTag.doStartTag();
+        scriptTag.doEndTag();
+    }
+    
+    /**
+     * @see br.com.concepting.framework.web.taglibs.BaseActionFormElementTag#clearAttributes()
+     */
+    protected void clearAttributes(){
+        super.clearAttributes();
+        
+        setAction("");
+        setForward("");
+        setForwardOnFail("");
+        setUpdateViews("");
+        setValidate(false);
+        setValidateProperties("");
+        setMilliseconds(0);
+    }
+}
