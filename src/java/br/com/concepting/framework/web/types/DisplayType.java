51cabc6bb2aace6280a0ca654d72ad45a7841b77
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-03 10:27:56 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/web/types/DisplayType.java
@@ -0,0 +1,14 @@
+package br.com.concepting.framework.web.types;
+
+/**
+ * Classe que define as constantes dos modos de renderização de um objeto/componente.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public enum DisplayType{
+    /**
+     * Constante que define que o objeto/componente não está renderizado.
+     */
+    NONE,
+}
