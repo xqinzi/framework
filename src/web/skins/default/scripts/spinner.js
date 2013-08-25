aaf3264ba160327739bc6adde5cd7328eeed157b
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-25 13:43:34 -0300

    Documentation.

--- src/web/skins/default/scripts/spinner.js
+++ src/web/skins/default/scripts/spinner.js
@@ -1,3 +1,18 @@
+/**
+ * Arquivo que contém as funções/propriedades para manipulação do componente visual spinner (controle de valores numéricos).
+ * 
+ * @author fvilarinho
+ * @version 1.0
+ */
+
+/**
+ * Adiciona o tamanho do passo no valor atual do componente respeitando o 
+ * seu valor máximo estabelecido.
+ * 
+ * @param name String contendo o identificador do componente.
+ * @param maximumValue Valor máximo do componente.
+ * @param step Valor numérico contendo o tamanho do passo.
+ */
 function addSpinnerValue(name, maximumValue, step){
 	var object = getObject(name);
 	
@@ -16,6 +31,14 @@ function addSpinnerValue(name, maximumValue, step){
 	}
 }
  
+/**
+ * Subtrai o tamanho do passo no valor atual do componente respeitando o 
+ * seu valor mínimo estabelecido.
+ * 
+ * @param name String contendo o identificador do componente.
+ * @param minimumValue Valor mínimo do componente.
+ * @param step Valor numérico contendo o tamanho do passo.
+ */
 function subtractSpinnerValue(name, minimumValue, step){
 	var object = getObject(name);
 	
