20b7f38742ecc2e1fcc8879933e70a7d2b0b1124
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-26 18:19:24 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/util/types/FormulaType.java
@@ -0,0 +1,51 @@
+package br.com.concepting.framework.util.types;
+
+/** 
+ * Classe que define as constantes para os tipos de fórmula suportados.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public enum FormulaType{
+    NONE,
+    SUM,
+    MIN,
+    MAX,
+    AVERAGE("avg");
+    
+    private String id;
+    
+    /**
+     * Construtor - Inicializa objetos e/ou variáveis internas.
+     */
+    private FormulaType(){
+        setId(this.name().toLowerCase());
+    }
+    
+    /**
+     * Construtor - Define o valor da constante.
+     * 
+     * @param id String contendo o identificador da fórmula.
+     */
+    private FormulaType(String id){
+        setId(id);
+    }
+
+    /**
+     * Retorna o identificador do tipo da fórmula.
+     * 
+     * @return String contendo o identificador do tipo da fórmula.
+     */
+    public String getId(){
+        return id;
+    }
+
+    /**
+     * Define o identificador do tipo da fórmula.
+     * 
+     * @param id String contendo o identificador do tipo da fórmula.
+     */
+    public void setId(String id){
+        this.id = id;
+    }
+}
