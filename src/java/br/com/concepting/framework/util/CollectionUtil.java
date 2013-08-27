48fc204be30c8ed16250d97c08fe02faa3c41ebe
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-27 19:26:09 -0300

    Documentation.

--- src/java/br/com/concepting/framework/util/CollectionUtil.java
+++ src/java/br/com/concepting/framework/util/CollectionUtil.java
@@ -5,8 +5,24 @@ import java.util.Collection;
 
 import org.apache.commons.beanutils.ConstructorUtils;
 import org.apache.commons.collections.CollectionUtils;
-
+ 
+/**
+ * Classe utilitária para manipulação de coleções.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
 public class CollectionUtil extends CollectionUtils{
+    /**
+     * Clona uma coleção.
+     * 
+     * @param source Instância da coleção.
+     * @return Coleção clonada.
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     */
     public static <O, C extends Collection<O>> C clone(C source) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException{
         C target = null;
 
