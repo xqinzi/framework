a6d9a3a572229bfb29a771f21720d6df2c4ca78d
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-27 15:32:32 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/processors/constants/ProcessorConstants.java
@@ -0,0 +1,19 @@
+package br.com.concepting.framework.processors.constants;
+
+/**
+ * Classe que define as constantes utilizadas pelas rotinas de processamento de conteúdo.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public abstract class ProcessorConstants{
+    /**
+     * Constante que define a tag de substituição de expressão regular que identifica qualquer caracter.
+     */
+    public static final String REGEX_ANY_TAG = "[any]";
+    
+    /**
+     * Constante que define a tag de remoção de conteúdo.
+     */
+    public static final String REMOVE_TAG = "[remove]";
+}
