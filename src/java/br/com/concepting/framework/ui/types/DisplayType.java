d2c8b58418b2ece7a5fdcd8af5d1df847f1dc09e
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-08-28 19:19:28 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/ui/types/DisplayType.java
@@ -0,0 +1,19 @@
+package br.com.concepting.framework.ui.types;
+
+/**
+ * Classe que define as constantes dos modos de renderização de um objeto/componente.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public enum DisplayType{
+    /**
+     * Constante que define que o objeto/componente não está renderizado.
+     */
+    NONE,
+    
+    /**
+     * Constante que define que o objeto/componente será renderizado sem quebra de linha..
+     */
+    NOWRAP;
+}
---
+++ src/java/br/com/concepting/framework/ui/types/VisibilityType.java
@@ -0,0 +1,19 @@
+package br.com.concepting.framework.ui.types;
+
+/**
+ * Classe que define as constantes dos modos de visibilidade de um objeto/componente.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */
+public enum VisibilityType{
+    /**
+     * Constante que define que o objeto/componente está escondido.
+     */
+    HIDDEN,
+    
+    /**
+     * Constante que define que o objeto/componente está visível.
+     */
+    VISIBLE;
+}
