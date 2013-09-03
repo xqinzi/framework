b66899f2bf77284376219190a88c943d1d05a1fc
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-09-03 20:23:04 -0300

    Documentation.

--- src/java/br/com/concepting/framework/ui/taglibs/EditButtonTag.java
+++ src/java/br/com/concepting/framework/ui/taglibs/EditButtonTag.java
@@ -3,7 +3,7 @@ package br.com.concepting.framework.ui.taglibs;
 import br.com.concepting.framework.controller.action.types.ActionType;
 
 /**
- * Classe que define o componente visual para o botão com a função editar um item selecionado.
+ * Classe que define o componente visual editButton (botão editar).
  * 
  * @author fvilarinho
  * @since 1.0
