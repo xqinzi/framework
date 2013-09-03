3b21c2a86bbe446f62e101a78908390229bae79f
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-09-03 20:16:45 -0300

    Code improvement.

--- src/java/br/com/concepting/framework/ui/taglibs/CancelButtonTag.java
+++ src/java/br/com/concepting/framework/ui/taglibs/CancelButtonTag.java
@@ -3,7 +3,7 @@ package br.com.concepting.framework.ui.taglibs;
 import br.com.concepting.framework.controller.action.types.ActionType;
 
 /**
- * Classe que define o componente visual para o botão com a função de cancelar.
+ * Classe que define o componente visual cancelButton (botão com a função de cancelar).
  * 
  * @author fvilarinho
  * @since 1.0
