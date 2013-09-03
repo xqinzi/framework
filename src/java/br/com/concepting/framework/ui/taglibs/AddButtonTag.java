344f1b9a829bf4c64c0c0d39d2868deecd78bd3b
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-09-03 19:41:25 -0300

    Documentation.

--- src/java/br/com/concepting/framework/ui/taglibs/AddButtonTag.java
+++ src/java/br/com/concepting/framework/ui/taglibs/AddButtonTag.java
@@ -3,7 +3,7 @@ package br.com.concepting.framework.ui.taglibs;
 import br.com.concepting.framework.controller.action.types.ActionType;
 
 /**
- * Classe que define o componente visual para o botão com a função adicionar um novo item.
+ * Classe que define o componente visual addButton (botão com a função adicionar um novo item).
  * 
  * @author fvilarinho
  * @since 1.0
