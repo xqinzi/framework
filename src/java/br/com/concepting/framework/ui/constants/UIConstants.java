a57e4f740c255efeb821c6d726af1437fbfc1130
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-09-07 15:02:46 -0300

    Code improvement.

---
+++ src/java/br/com/concepting/framework/ui/constants/UIConstants.java
@@ -0,0 +1,29 @@
+package br.com.concepting.framework.ui.constants;
+
+/**
+ * Classe que define as constantes utilizadas na camada de apresentação.
+ * 
+ * @author fvilarinho
+ * @since 3.0
+ */ 
+public abstract class UIConstants{
+    /**
+     * Constante que define a URL default da página de erros default. 
+     */
+    public static final String DEFAULT_ERROR_PAGE = "/errorPage.jsp";
+    
+    /**
+     * Constante que define o diretório de armazenamento default dos scripts de página.
+     */
+    public static final String DEFAULT_SCRIPTS_RESOURCES_DIR = "/scripts/";
+
+    /**
+     * Constante que define o diretório de armazenamento default dos estilos de página.
+     */
+    public static final String DEFAULT_STYLES_RESOURCES_DIR = "/styles/";
+
+    /**
+     * Constante que define o diretório de armazenamento default das imagens da página.
+     */
+    public static final String DEFAULT_IMAGES_RESOURCES_DIR = "/images/";
+}
