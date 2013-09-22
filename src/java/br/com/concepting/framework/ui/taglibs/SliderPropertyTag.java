0aee8cbbb9560793908866b413a8c9f7cf15311c
Felipe Vilarinho <fvilarinho@concepting.com.br>
2013-09-21 22:45:43 -0300

    Code improvement.

--- src/java/br/com/concepting/framework/ui/taglibs/SliderPropertyTag.java
+++ src/java/br/com/concepting/framework/ui/taglibs/SliderPropertyTag.java
@@ -1,6 +1,8 @@
 package br.com.concepting.framework.ui.taglibs;
 
+import br.com.concepting.framework.model.helpers.PropertyInfo;
 import br.com.concepting.framework.ui.constants.TaglibConstants;
+import br.com.concepting.framework.util.NumberUtil;
  
 /**
  * Classe que define o componente visual slider (barra de deslize para controle de valores numéricos). 
@@ -8,28 +10,8 @@ import br.com.concepting.framework.ui.constants.TaglibConstants;
  * @author fvilarinho
  * @since 3.0
  */
-public class SliderPropertyTag extends BasePropertyTag{
+public class SliderPropertyTag extends TextPropertyTag{
     private Long maximumValue = null;
-    private Long minimumValue = null;
-    private Long step         = 1l;
-    
-    /**
-     * Retorna o tamanho do passo a ser acrescido ou diminuído do valor.
-     * 
-     * @return Valor numérico contendo o tamanho do passo.
-     */
-    public Long getStep(){
-        return step;
-    }
-
-    /**
-     * Define o tamanho do passo a ser acrescido ou diminuído do valor.
-     * 
-     * @param step Valor numérico contendo o tamanho do passo.
-     */
-    public void setStep(Long step){
-        this.step = step;
-    }
 
     /**
      * Retorna o valor máximo a ser alcançado.
@@ -48,72 +30,134 @@ public class SliderPropertyTag extends BasePropertyTag{
     public void setMaximumValue(Long maximumValue){
         this.maximumValue = maximumValue;
     }
-
-    /**
-     * Retorn o valor mínimo a ser alcançado.
-     * 
-     * @return Valor numérico mínimo a ser alcançado.
-     */
-    public Long getMinimumValue(){
-        return minimumValue;
-    }
-
-    /**
-     * Define o valor mínimo a ser alcançado.
-     * 
-     * @param minimumValue Valor numérico mínimo a ser alcançado.
-     */
-    public void setMinimumValue(Long minimumValue){
-        this.minimumValue = minimumValue;
-    }
     
     /**
      * @see br.com.concepting.framework.ui.taglibs.BasePropertyTag#renderBody()
      */
     protected void renderBody() throws Throwable{
-        String name = getName();
+        if(isRendered()){
+            PropertyInfo propertyInfo = getPropertyInfo();
+            
+            if(propertyInfo != null && propertyInfo.isNumber()){
+                println("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">");
+                
+                println("<tr>");
+                
+                println("<td>");
+                
+                String name = getName();
         
-        println("<div>");
+                println("<div>");
+                
+                print("<div id=\"");
+                print(name);
+                print(".");
+                print(TaglibConstants.SLIDER_BAR_CONTROL_ID);
+                print("\" class=\"");
+                print(TaglibConstants.DEFAULT_SLIDER_BAR_CONTROL_STYLE_CLASS);
+                print("\"");
+                
+                if(getEnabled() && !getReadOnly()){
+                    print(" onMouseDown=\"dragSliderBarControl('");
+                    print(name);
+                    print("', event);\" onMouseUp=\"dropSliderBarControl();\" onMouseOut=\"dropSliderBarControl();\" onMouseMove=\"slideIt(event);\"");
+                }
+                
+                println("></div>");
         
-        print("<div id=\"");
-        print(name);
-        print(".");
-        print(TaglibConstants.DEFAULT_SLIDER_BAR_CONTROL_STYLE_CLASS);
-        print("\" class=\"");
-        print(TaglibConstants.DEFAULT_SLIDER_BAR_CONTROL_STYLE_CLASS);
-        print("\" onMouseDown=\"dragSliderBar(this, event);\" onMouseOut=\"dropSliderBar(this, event);\" onMouseUp=\"dropSliderBar(this, event);\"></div>");
+                print("<div id=\"");
+                print(name);
+                print(".");
+                print(TaglibConstants.SLIDER_BAR_ID);
+                print("\" class=\"");
+                print(TaglibConstants.DEFAULT_SLIDER_BAR_STYLE_CLASS);
+                print("\"></div>");
         
-        print("<div id=\"");
-        print(name);
-        print(".");
-        print(TaglibConstants.DEFAULT_SLIDER_BAR_STYLE_CLASS);
-        print("\" class=\"");
-        print(TaglibConstants.DEFAULT_SLIDER_BAR_STYLE_CLASS);
-        println("\">");
+                println("</div>");
+                
+                println("</td>");
+                
+                println("<td width=\"5\"></td>");
         
-        print("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" width=\"");
-        print(getWidth());
-        println("\">");
+                println("<td>");
+                
+                super.renderBody();
+                
+                println("</td>");
+                
+                println("</tr>");
+                
+                println("</table>");
+                
+                String        width         = getWidth();  
+                StringBuilder scriptContent = new StringBuilder();
+                
+                scriptContent.append("initializeSlider('");
+                scriptContent.append(name);
+                scriptContent.append("', ");
+                scriptContent.append(NumberUtil.parseInt(width));
+                scriptContent.append(", ");
+                scriptContent.append(maximumValue);
+                scriptContent.append(", ");
+                scriptContent.append(getValue());
+                scriptContent.append(");");
+                
+                ScriptTag scriptTag = new ScriptTag();
+                
+                scriptTag.setPageContext(pageContext);
+                scriptTag.setContent(scriptContent.toString());
+                scriptTag.doStartTag();
+                scriptTag.doEndTag();
+            }
+            else{
+                print("<span class=\"");
+                print(TaglibConstants.DEFAULT_LABEL_STYLE_CLASS);
+                println("\">");
+                println(getInvalidPropertyMessage());
+                println("</span>");
+            }
+        }
+    }
+    
+    /**
+     * @see br.com.concepting.framework.ui.taglibs.TextPropertyTag#initialize()
+     */
+    protected void initialize() throws Throwable{
+        String width = getWidth();
         
-        println("<tr>");
+        setWidth("");
         
-        print("<td class=\"");
-        print(TaglibConstants.DEFAULT_LEFT_SLIDER_BAR_STYLE_CLASS);
-        println("\"></td>");
-        print("<td class=\"");
-        print(TaglibConstants.DEFAULT_SLIDER_BAR_BODY_STYLE_CLASS);
-        println("\"></td>");
-        print("<td class=\"");
-        print(TaglibConstants.DEFAULT_RIGHT_SLIDER_BAR_STYLE_CLASS);
-        println("\"></td>");
+        super.initialize();
         
-        println("</tr>");
+        PropertyInfo propertyInfo = getPropertyInfo();
         
-        println("</table>");
-        
-        println("</div>");
-        
-        println("</div>");
+        if(propertyInfo.isNumber()){
+            setWidth(width);
+    
+            if(maximumValue == null)
+                maximumValue = NumberUtil.getMaximumRange(propertyInfo.getClazz()).longValue();
+            
+            setSize((int)(Math.log10(maximumValue)));
+            setMaxlength(getSize());
+    
+            StringBuilder onBlurContent = new StringBuilder();
+            
+            onBlurContent.append("setSliderPosition('");
+            onBlurContent.append(getName());
+            onBlurContent.append("');");
+            
+            String onBlur = getOnBlur();
+            
+            if(onBlur.length() > 0){
+                onBlurContent.append(" ");
+                onBlurContent.append(onBlur);
+                
+                if(!onBlur.endsWith(";"))
+                    onBlurContent.append(";");
+            }
+            
+            setOnBlur(onBlurContent.toString());
+        }
     }
  
     /**
@@ -123,7 +167,5 @@ public class SliderPropertyTag extends BasePropertyTag{
         super.clearAttributes();
         
         setMaximumValue(null);
-        setMinimumValue(null);
-        setStep(1l);
     }
 }
